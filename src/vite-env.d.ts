b0a09da208faf689585a3dabef1329a3244f52f6
MaxDevs <underworldhacker7@gmail.com>
2025-11-12 13:42:31 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GEMINI_BOT_API_KEY: string
+  // Add other VITE_ prefixed environment variables here
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}/// <reference types="vite/client" />
