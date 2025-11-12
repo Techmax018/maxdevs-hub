f894553d2ac85f94ef29f460eec4f470945e0cec
MaxDevs <underworldhacker7@gmail.com>
2025-11-12 13:43:02 +0300

    Rename VITE_GEMINI_BOT_API_KEY to GEMINI_BOT_API_KEY

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_GEMINI_BOT_API_KEY: string
+  readonly GEMINI_BOT_API_KEY: string
   // Add other VITE_ prefixed environment variables here
 }
 
